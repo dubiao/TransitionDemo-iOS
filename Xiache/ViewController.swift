9cd74a2d860a80484358aa58fea10733b81e6114
杜标 <bill.du@cleargrass.com>
2017-12-26 12:31:05 +0800

    Initial Commit

---
+++ Xiache/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xiache
+//
+//  Created by 杜标 on 2017/12/26.
+//  Copyright © 2017年 weakup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
